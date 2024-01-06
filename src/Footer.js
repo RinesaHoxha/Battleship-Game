0ae84226ddc298cf8ca10a53e3183507c0f2e832
KlajdiGashi <klajdi.gashi@student.uni-pr.edu>
2024-01-06 20:07:00 +0000

    BG-24

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <footer>
+    Made by <a href="https://github.com/RinesaHoxha/Battleship-Game">grupi ma i mire i fiekut! - R. H 2024.01.01</a>{' '}  {' '}
+    </footer>
+  );
+};
