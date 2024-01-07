1b8a607595340f8a24fccc5a24c90448e1cc10cc
rinesan <117470993+rinesan@users.noreply.github.com>
2024-01-07 12:42:59 +0100

    BG 12

--- Game/PlayerBoard.js
+++ Game/PlayerBoard.js
@@ -39,3 +39,50 @@ export const PlayerBoard = ({
         : prevLayout,
     layout
   );
+  const isPlacingOverBoard = currentlyPlacing && currentlyPlacing.position != null;
+  const canPlaceCurrentShip = isPlacingOverBoard && canBePlaced(currentlyPlacing, layout);
+
+  if (isPlacingOverBoard) {
+    if (canPlaceCurrentShip) {
+      layout = putEntityInLayout(layout, currentlyPlacing, SQUARE_STATE.ship);
+    } else {
+      let forbiddenShip = {
+        ...currentlyPlacing,
+        length: currentlyPlacing.length - calculateOverhang(currentlyPlacing),
+      };
+      layout = putEntityInLayout(layout, forbiddenShip, SQUARE_STATE.forbidden);
+    }
+  }
+
+  let squares = layout.map((square, index) => {
+    return (
+      <div
+        onMouseDown={rotateShip}
+        onClick={() => {
+          if (canPlaceCurrentShip) {
+            playSound('click');
+            placeShip(currentlyPlacing);
+          }
+        }}
+        className={`square ${stateToClass[square]}`}
+        key={`square-${index}`}
+        id={`square-${index}`}
+        onMouseOver={() => {
+          if (currentlyPlacing) {
+            setCurrentlyPlacing({
+              ...currentlyPlacing,
+              position: indexToCoords(index),
+            });
+          }
+        }}
+      />
+    );
+  });
+
+  return (
+    <div>
+      <h2 className="player-title">You</h2>
+      <div className="board">{squares}</div>
+    </div>
+  );
+};
