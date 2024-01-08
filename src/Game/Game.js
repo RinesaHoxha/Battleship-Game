6b50965382fd90caf77f1a8167288520ab3c3e9d
Mariam Mourad <108888673+mariam1mourad@users.noreply.github.com>
2024-01-08 20:19:23 +0100

    BG-9

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,6 +1,33 @@
 import React, { useState, useRef } from 'react';
 
-//available ships here
+const AVAILABLE_SHIPS = [
+  {
+    name: 'carrier',
+    length: 5,
+    placed: null,
+  },
+  {
+    name: 'battleship',
+    length: 4,
+    placed: null,
+  },
+  {
+    name: 'cruiser',
+    length: 3,
+    placed: null,
+  },
+  {
+    name: 'submarine',
+    length: 3,
+    placed: null,
+  },
+  {
+    name: 'destroyer',
+    length: 2,
+    placed: null,
+  },
+];
+
 export const Game = () => {
     const [gameState, setGameState] = useState('placement');
     const [winner, setWinner] = useState(null);
