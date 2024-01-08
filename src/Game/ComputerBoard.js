dce01e93063b56f6b6b2a3d1986e0d8aab5056a2
KlajdiGashi <klajdi.gashi@student.uni-pr.edu>
2024-01-08 19:39:09 +0000

    BG-30

--- src/Game/ComputerBoard.js
+++ src/Game/ComputerBoard.js
@@ -8,3 +8,26 @@ import {
   updateSunkShips,
 } from './layoutHelpers';
 
+export const ComputerBoard = ({
+    computerShips,
+    gameState,
+    hitsByPlayer,
+    setHitsByPlayer,
+    handleComputerTurn,
+    checkIfGameOver,
+    setComputerShips,
+    playSound,
+  }) => {
+    // Ships on an empty layout
+    let compLayout = computerShips.reduce(
+      (prevLayout, currentShip) =>
+        putEntityInLayout(prevLayout, currentShip, SQUARE_STATE.ship),
+      generateEmptyLayout()
+    );
+  
+    //  Add hits dealt by player
+    compLayout = hitsByPlayer.reduce(
+      (prevLayout, currentHit) =>
+        putEntityInLayout(prevLayout, currentHit, currentHit.type),
+      compLayout
+    );
