92c735226d591fe74c2dba90d90a7c2ffdf3b459
Ilirjana Suka <118145049+IlirjanaSuka@users.noreply.github.com>
2024-01-09 20:37:27 +0100

    BG-37

--- src/Game/GameView.js
+++ src/Game/GameView.js
@@ -1,1 +1,40 @@
 import React from 'react';
+
+import { PlayerFleet } from './PlayerFleet';
+import { PlayerBoard } from './PlayerBoard';
+import { ComputerBoard } from './ComputerBoard';
+import { PlayerTips } from './PlayerTips';
+
+export const GameView = ({
+  availableShips,
+  selectShip,
+  currentlyPlacing,
+  setCurrentlyPlacing,
+  rotateShip,
+  placeShip,
+  placedShips,
+  startTurn,
+  computerShips,
+  gameState,
+  changeTurn,
+  hitComputer,
+  hitsByPlayer,
+  setHitsByPlayer,
+  hitsByComputer,
+  handleComputerTurn,
+  checkIfGameOver,
+  winner,
+  startAgain,
+  setComputerShips,
+  playSound,
+}) => {
+  return (
+  
+        />
+      ) : (
+       
+      )}
+
+   </section>
+  );
+};
