ea1e204cf2fc1f69af94edd334865364e61722b6
rinesan <117470993+rinesan@users.noreply.github.com>
2024-01-10 19:51:11 +0100

    BG 28

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -32,8 +32,44 @@ export const Game = () => {
     const [gameState, setGameState] = useState('placement');
     const [winner, setWinner] = useState(null);
 
-    //game functions here
+  // *** PLAYER ***
+  const selectShip = (shipName) => {
+    let shipIdx = availableShips.findIndex((ship) => ship.name === shipName);
+    const shipToPlace = availableShips[shipIdx];
 
+    setCurrentlyPlacing({
+      ...shipToPlace,
+      orientation: 'horizontal',
+      position: null,
+    });
+  };
+
+  const placeShip = (currentlyPlacing) => {
+    setPlacedShips([
+      ...placedShips,
+      {
+        ...currentlyPlacing,
+        placed: true,
+      },
+    ]);
+
+    setAvailableShips((previousShips) =>
+      previousShips.filter((ship) => ship.name !== currentlyPlacing.name)
+    );
+
+    setCurrentlyPlacing(null);
+  };
+
+  const rotateShip = (event) => {
+    if (currentlyPlacing != null && event.button === 2) {
+      setCurrentlyPlacing({
+        ...currentlyPlacing,
+        orientation:
+          currentlyPlacing.orientation === 'vertical' ? 'horizontal' : 'vertical',
+      });
+    }
+  };
+  
     return (//everything needed to update game view here
         <React.Fragment>
             <GameView>
