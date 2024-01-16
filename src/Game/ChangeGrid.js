2577e5ce53465c4f59e968b75fe0a87427a64903
Ilirjana Suka <118145049+IlirjanaSuka@users.noreply.github.com>
2024-01-16 21:56:28 +0100

    Update ChangeGrid.js

---
+++ src/Game/ChangeGrid.js
@@ -0,0 +1,51 @@
+import {
+    generateEmptyLayout,
+    SQUARE_STATE,
+    BOARD_ROWS,
+    BOARD_COLUMNS,
+    indexToCoords,
+    putEntityInLayout,
+    generateRandomIndex,
+    getNeighbors,
+    updateSunkShips,
+    coordsToIndex,
+  } from './layoutHelpers';
+
+import {
+    placedShips,
+    AVAILABLE_SHIPS,
+} from './Game';
+
+
+export const getHighestValue = (grid) => {
+    let highestValue = {x: 0, y: 0}; //Number.MIN_SAFE_INTEGER
+    for(let x = 0; x < BOARD_ROWS; x++) {
+        for(let y = 0; y < BOARD_COLUMNS; y++) {
+            let indexFromCoords = coordsToIndex({x: x, y: y});
+            if(grid[indexFromCoords] > highestValue) {
+                highestValue = {x: x, y: y}
+            }
+        }
+    }
+    return highestValue;
+}
+
+export const divideGrid = (grid, divisor) => {
+    return grid.map(value => value / divisor);
+}
+
+export const incrementCells = (grid, {x, y}, incrementedBy) => {
+    let indexFromCoords = coordsToIndex({x: x, y: y});
+    grid[indexFromCoords] += incrementedBy;
+    return grid;
+}
+
+export const incrementAll = (grid) => {
+    for(let x = 0; x < BOARD_ROWS; x++) {
+        for(let y = 0; y < BOARD_COLUMNS; y++) {
+            let indexFromCoords = coordsToIndex({x: x, y: y});
+            grid[indexFromCoords] += 1;
+        }
+    }
+    return grid;
+};
