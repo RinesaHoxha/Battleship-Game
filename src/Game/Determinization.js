76fbc0422b21792174b83437ec869f84864487c1
myhedinv <myhedin.vuciterna@student.uni-pr.edu>
2024-01-17 08:05:24 +0100

    BG 50

--- src/Game/Determinization.js
+++ src/Game/Determinization.js
@@ -7,14 +7,14 @@ import {
     canBePlaced,
     indexToCoords,
   } from './layoutHelpers';
-
+  
   import {
     AVAILABLE_SHIPS,
   } from './Game';
-
-const totalIndex = BOARD_ROWS * BOARD_COLUMNS;
-export const validDeterminization = (informationGrid, possibleGrid) => {
-  for(let index = 0; index < totalIndex; index++) {
+  
+  const totalIndex = BOARD_ROWS * BOARD_COLUMNS;
+  export const validDeterminization = (informationGrid, possibleGrid) => {
+   for(let index = 0; index < totalIndex; index++) {
     if(informationGrid[index] === SQUARE_STATE.hit) {
       if(!(possibleGrid[index] === SQUARE_STATE.ship)) {
         return false;
@@ -24,60 +24,60 @@ export const validDeterminization = (informationGrid, possibleGrid) => {
         return false;
       }
     }
-  }
+   }
     return true;
   };
-
+  
   export const reproduceShots = (informationGrid, possibleGrid) => {
     for(let index = 0; index < totalIndex; index++) {
       if(informationGrid[index] === SQUARE_STATE.hit) 
         {
-        possibleGrid[index] = SQUARE_STATE.hit;
+         possibleGrid[index] = SQUARE_STATE.hit;
         } else if(informationGrid[index] === SQUARE_STATE.miss) 
         possibleGrid[index] = SQUARE_STATE.miss;
     }
     return possibleGrid;
   };
-
-export const makeRandomDeterminization = (informationGrid) => {
+  
+  export const makeRandomDeterminization = (informationGrid) => {
   let possibleGrid = generateEmptyLayout();
   possibleGrid = reproduceShots(informationGrid, possibleGrid);
-
+  
   let availShips = AVAILABLE_SHIPS.slice();
   for (let index = 0; index < totalIndex; index++) {
       if(informationGrid[index] === SQUARE_STATE.hit && (possibleGrid[index] === SQUARE_STATE.empty)) {
         if (availShips.length === 0) {
           return possibleGrid;
         }
-
+  
         let randomShipIndex = Math.floor(Math.random() * availShips.length);
         let ship = availShips[randomShipIndex];
         let shipEntity = null;
-
+  
         for(let shipOrient = 0; shipOrient < 2; shipOrient++) {
           shipEntity = {...ship, position : indexToCoords(index), orientation : Math.floor(Math.random() * 2) === 1 ? 'vertical' : 'horizontal'};
           const canPlaceCurrentShip = canBePlaced(shipEntity, possibleGrid);
-
+  
           if (canPlaceCurrentShip) {
             possibleGrid = putEntityInLayout(possibleGrid, shipEntity, SQUARE_STATE.ship);
             availShips.splice(randomShipIndex, 1);
           }
-
+  
           break;
         }
       }
   }
-
+  
   while(availShips.length > 0) { // send help OMG
     let randomIndex = Math.floor(Math.random() * totalIndex);
-
+  
     if (possibleGrid[randomIndex] != SQUARE_STATE.ship) {
       let randomShipIndex = Math.floor(Math.random() * availShips.length);
       let ship = availShips[randomShipIndex];
       let shipEntity = {...ship, position : indexToCoords(randomIndex), orientation : Math.floor(Math.random() * 2) == 1 ? 'horizontal' : 'vertical'};
-
+  
       const canPlaceCurrentShip = canBePlaced(shipEntity, possibleGrid);
-
+  
       if (canPlaceCurrentShip) {
         possibleGrid = putEntityInLayout(possibleGrid, shipEntity, SQUARE_STATE.ship);
         availShips.splice(randomShipIndex, 1);
@@ -85,10 +85,10 @@ export const makeRandomDeterminization = (informationGrid) => {
     }
   }
   return possibleGrid;
-};
-
+  };
+  
   export const makePossibleDeterminization = (informationGrid, depth = 0) => {
-    const MAX_DEPTH = 50; 
+    const MAX_DEPTH = 20; 
     if (depth > MAX_DEPTH) {
       //return informationGrid; //cheating ai
       return makeRandomDeterminization(informationGrid);// duct tape fix
@@ -102,24 +102,24 @@ export const makeRandomDeterminization = (informationGrid) => {
   
     return makePossibleDeterminization(informationGrid, depth + 1); //recursive
   }
-
-export const makeDeterminization = (currentCompFires, iterations) => {
+  
+  export const makeDeterminization = (currentCompFires, iterations) => {
   let determinization = [];
   const startTime = new Date(); // idk so it doesn't take too long.
-
+  
   for(let i = 0; i < iterations; i++) {
     let informationGrid = currentCompFires;
     let possibleGrid = makePossibleDeterminization(informationGrid);
     determinization.push(possibleGrid);
-
+  
     const endTime = new Date();
     const timePassed = endTime.getTime() - startTime.getTime(); // is in ms
-
+  
     if (timePassed > 500) {
       break;
     }
   }
   
   return determinization;
-}
-
+  }
+  
